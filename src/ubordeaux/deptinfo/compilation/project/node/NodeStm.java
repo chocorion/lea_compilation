58e311e23ec0cae8af919fa9ef087dccd575e8a9
Romain Verge <rverge002@emi.u-bordeaux1.fr>
2019-04-25 16:13:42 +0200

    creation de nodestm

---
+++ src/ubordeaux/deptinfo/compilation/project/node/NodeStm.java
@@ -0,0 +1,26 @@
+package ubordeaux.deptinfo.compilation.project.node;
+
+import ubordeaux.deptinfo.compilation.project.type.Type;
+
+public abstract class NodeStm extends Node {
+
+	protected Type type;
+	protected Stm stm;
+
+	public NodeStm(Node... abstTrees) {
+		super(abstTrees);
+	}
+
+	public Type getType() {
+		return this.type;
+	}
+
+	public void setType(Type t) {
+		this.type = t;
+	}
+
+	public Stm getStm() {
+		return this.stm;
+	}
+
+}
